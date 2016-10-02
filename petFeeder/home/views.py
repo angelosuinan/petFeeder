6b66593c43df1acaf70fb88437b9b93d34d8d9b3
angelosuinan <suinanangelo@gmail.com>
2016-10-02 08:21:28 +0000

    created homepage

--- petFeeder/home/views.py
+++ petFeeder/home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.views.generic import View
 # Create your views here.
+class Index(View):
+        template_name = 'home/index.html'
+       	def get(self, request, *args, **kwargs):
+            return render(request, self.template_name,)
